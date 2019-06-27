22c6c291e06957ab7c17adca651ef5965e0d9a72
David <digital0david@gmail.com>
2019-06-27 17:08:42 -0400

    display/dashboard/controls spec start

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react'
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls.js';
+
+afterEach(rtl.cleanup);
+
+descrive('<Controls />', () => {
+
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+import React from 'react';
+import * as rtl from 'react-testing-library'
+import renderer from 'react-test-renderer'
+
+import Dashboard from './Dashboard.js';
+import Display from '../display/Display.js';
+import Controls from '../controls/Controls.js';
+
+afterEach(rtl.cleanup)
+
+describe('<Dashboard />', () => {
+
+})
