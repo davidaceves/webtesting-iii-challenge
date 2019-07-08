6b449deac982db3eb7a1fb754bc622872ffe5bff
David <digital0david@gmail.com>
2019-07-07 22:54:55 -0400

    cleanup

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,18 +1,10 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import "jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
 
 import Display from "./Display.js";
-import Controls from "../controls/Controls.js";
 import Dashboard from "../dashboard/Dashboard.js";
-
-// displays if gate is open/closed and if it is locked/unlocked DONE
-// displays 'Closed' if the closed prop is true and 'Open' if otherwise DONE
-// displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise DONE
-// when locked or closed use the red-led class
-// when unlocked or open use the green-led class
 
 describe("<Display />", () => {
   it("displays if gate is open/closed", () => {
