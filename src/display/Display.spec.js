fd9cb0b59481011438b1fcf9f5810d3c1a25c608
David <digital0david@gmail.com>
2019-07-07 22:00:25 -0400

    locked/unlocked prop test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,6 +8,12 @@ import Display from "./Display.js";
 import Controls from "../controls/Controls.js";
 import Dashboard from "../dashboard/Dashboard.js";
 
+// displays if gate is open/closed and if it is locked/unlocked DONE
+// displays 'Closed' if the closed prop is true and 'Open' if otherwise DONE
+// displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
+// when locked or closed use the red-led class
+// when unlocked or open use the green-led class
+
 describe("<Display />", () => {
   it("displays if gate is open/closed", () => {
     const { getByTestId } = render(<Dashboard />);
@@ -39,5 +45,21 @@ describe("<Display />", () => {
     const gate = getByTestId("closed_open");
 
     expect(gate).toHaveTextContent(/Open/i);
+  });
+
+  it("displays 'Locked' if the locked prop is `true`", () => {
+    const { getByTestId } = render(<Display locked={true} />);
+
+    const gate = getByTestId("locked_unlocked");
+
+    expect(gate).toHaveTextContent(/Locked/i);
+  });
+
+  it("displays 'Unlocked' if the locked prop is `false`", () => {
+    const { getByTestId } = render(<Display locked={false} />);
+
+    const gate = getByTestId("locked_unlocked");
+
+    expect(gate).toHaveTextContent(/Unlocked/i);
   });
 });
